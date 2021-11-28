f43fcf48e35789b0800b074ed597454080aa9819
Michi <tasaki1931@gmail.com>
2021-11-28 10:28:45 +0900

    6st commit

--- app.py
+++ app.py
@@ -1,41 +1,20 @@
 import streamlit as st
 import pandas as pd
 
-st.title('My first app')
+# DataFrameを生成するcache機能付きの関数を定義
+@st.cache
+def get_df(s):
+    return pd.DataFrame({'id': ['1', '2', '3'], 'name': ['X', 'Y', s]})
 
-st.write('## テーブル')
-st.write('スパムが多い')
+# text formを生成
+s = st.text_input('input s')
+df = get_df(s)
 
-import streamlit as st
+# DataFrameをテーブルで表示
+st.markdown('# Table')
+st.table(df)
 
-'''
-# ウィジェット
-## input box
-テキストの変更もちゃんと反映される
-'''
-
-text = st.text_input('入力するよ', 'でふぉると')
-st.write(text)
-
-'''
-## check box
-チェックボックス
-'''
-
-if st.checkbox('ちぇっくぼっくす'):
-    st.write('オンだよ')
-else:
-    st.write('オフだよ')
-
-import streamlit as st
-import matplotlib.pyplot as plt
-import numpy as np
-import warnings
-warnings.filterwarnings('ignore')
-
-n = st.sidebar.number_input('n', min_value=2)
-
-x = np.linspace(0, n*np.pi, 100)
-plt.plot(x, np.sin(x))
-
+# histgramを表示
+st.markdown('# Histgram')
+df['name'].hist()
 st.pyplot()
