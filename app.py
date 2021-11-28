946abd4af3c65eaf62f9d18a3fa001f8d9d240f6
Michi <tasaki1931@gmail.com>
2021-11-28 09:30:23 +0900

    5st commit

--- app.py
+++ app.py
@@ -5,3 +5,37 @@ st.title('My first app')
 
 st.write('## テーブル')
 st.write('スパムが多い')
+
+import streamlit as st
+
+'''
+# ウィジェット
+## input box
+テキストの変更もちゃんと反映される
+'''
+
+text = st.text_input('入力するよ', 'でふぉると')
+st.write(text)
+
+'''
+## check box
+チェックボックス
+'''
+
+if st.checkbox('ちぇっくぼっくす'):
+    st.write('オンだよ')
+else:
+    st.write('オフだよ')
+
+import streamlit as st
+import matplotlib.pyplot as plt
+import numpy as np
+import warnings
+warnings.filterwarnings('ignore')
+
+n = st.sidebar.number_input('n', min_value=2)
+
+x = np.linspace(0, n*np.pi, 100)
+plt.plot(x, np.sin(x))
+
+st.pyplot()
